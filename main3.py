462710be1ca8dd7e4002904e0f61e12718930c5b
matsue-code <mbolamathew@icloud.com>
2025-08-12 20:52:30 +0300

    third commit

---
+++ main3.py
@@ -0,0 +1,20 @@
+num =int(input("enter the number:"))
+t = num
+numlen=0
+while t>0:
+    numlen = numlen+1
+    t=int(t/10)
+if numlen>=4:
+    numlen=int(numlen/2)
+    chk=0
+    while num>0:
+        rem =num%10
+        if chk==numlen:
+            midOne=rem
+        elif chk==(numlen-1):
+            midTwo =rem
+        num = int(num/10)
+        chk=chk+1
+    prod=midOne*midTwo
+    print("\nProduct of mid didgits(" + str(midOne)+ "*"+str(midTwo)+")=",prod)
+        
